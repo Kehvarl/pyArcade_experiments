b5e5116e1f2a0649760625e6330fe1843a780b31
Code Dragon <Kehvarl@gmail.com>
2018-04-20 22:20:57 -0700

    Some initial testing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pyArcade_experiments
+
+Tileset Information: "Dungeon Crawl 32x32 Tiles Supplemental"
+https://opengameart.org/content/dungeon-crawl-32x32-tiles-supplemental
+Original Source: http://crawl.develz.org/wordpress/
+
+
+
+
---
+++ demo.py
@@ -0,0 +1,154 @@
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.sprite_starting_template
+"""
+import arcade
+import os
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+MAZE_WIDTH = 40
+MAZE_HEIGHT = 30
+
+NATIVE_SPRITE_SIZE = 32
+SPRITE_SCALING = (SCREEN_HEIGHT / MAZE_HEIGHT) / NATIVE_SPRITE_SIZE
+SPRITE_SIZE = NATIVE_SPRITE_SIZE * SPRITE_SCALING
+
+
+class ArcadeDemo(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height):
+        super().__init__(width, height, "Roguelike Demo", resizable=True)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+        self.map_list = None
+        self.wall_textures = None
+        self.floor_textures = None
+
+    def setup(self):
+        # Create your sprites and sprite lists here
+        self.map_list = arcade.SpriteList()
+
+        self.wall_textures = []
+        self.wall_textures.append(arcade.load_texture(file_name="tileset/dungeon/wall/snake_0.png",
+                                                      scale=SPRITE_SCALING))
+        self.wall_textures.append(arcade.load_texture(file_name="tileset/dungeon/wall/snake_1.png",
+                                                      scale=SPRITE_SCALING))
+        self.wall_textures.append(arcade.load_texture(file_name="tileset/dungeon/wall/snake_2.png",
+                                                      scale=SPRITE_SCALING))
+        self.wall_textures.append(arcade.load_texture(file_name="tileset/dungeon/wall/snake_3.png",
+                                                      scale=SPRITE_SCALING))
+        self.wall_textures.append(arcade.load_texture(file_name="tileset/dungeon/wall/snake_4.png",
+                                                      scale=SPRITE_SCALING))
+
+        self.floor_textures = []
+        self.floor_textures.append(arcade.load_texture(file_name="tileset/dungeon/floor/mosaic_10.png",
+                                                       scale=SPRITE_SCALING))
+        self.floor_textures.append(arcade.load_texture(file_name="tileset/dungeon/floor/mosaic_11.png",
+                                                       scale=SPRITE_SCALING))
+        self.floor_textures.append(arcade.load_texture(file_name="tileset/dungeon/floor/mosaic_12.png",
+                                                       scale=SPRITE_SCALING))
+        self.floor_textures.append(arcade.load_texture(file_name="tileset/dungeon/floor/mosaic_13.png",
+                                                       scale=SPRITE_SCALING))
+        self.floor_textures.append(arcade.load_texture(file_name="tileset/dungeon/floor/mosaic_14.png",
+                                                       scale=SPRITE_SCALING))
+
+        for y in range(MAZE_HEIGHT):
+            for x in range(MAZE_WIDTH):
+                map_tile = arcade.Sprite()
+                if random.randint(1, 2) == 1:
+                    map_tile.texture = random.choice(self.wall_textures)
+                else:
+                    map_tile.texture = random.choice(self.floor_textures)
+                map_tile.center_x = x * SPRITE_SIZE + SPRITE_SIZE / 2
+                map_tile.center_y = y * SPRITE_SIZE + SPRITE_SIZE / 2
+
+                self.map_list.append(map_tile)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+
+        # Call draw() on all your sprite lists below
+        self.map_list.draw()
+
+    def update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        http://arcade.academy/arcade.color.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+
+def main():
+    """ Main method """
+    game = ArcadeDemo(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
